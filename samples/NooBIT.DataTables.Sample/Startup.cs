d9a4e5def6320547baa6e374e9fa993665ffb441
tobias.haimerl <tobias.haimerl@conceptnet.de>
2018-10-26 14:25:48 +0200

    add editorconfig. update packages

--- samples/NooBIT.DataTables.Sample/Startup.cs
+++ samples/NooBIT.DataTables.Sample/Startup.cs
@@ -21,7 +21,8 @@ namespace NooBIT.DataTables.Sample
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
             app.UseMvcWithDefaultRoute();
         }
     }
