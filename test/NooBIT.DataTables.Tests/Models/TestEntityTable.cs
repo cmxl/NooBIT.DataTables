d00b9990019f1ebd95f92d0204f05c40c54e6dd0
tobias.haimerl <tobias.haimerl@conceptnet.de>
2018-11-06 16:00:48 +0100

    little fixes

--- test/NooBIT.DataTables.Tests/Models/TestEntityTable.cs
+++ test/NooBIT.DataTables.Tests/Models/TestEntityTable.cs
@@ -1,7 +1,3 @@
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using NooBIT.DataTables.Models;
 using NooBIT.DataTables.Queries;
 
 namespace NooBIT.DataTables.Tests.Models
@@ -10,11 +6,6 @@ namespace NooBIT.DataTables.Tests.Models
     {
         public TestEntityTable(IQueryableRequestService<TestEntity> queryableRequestService) : base(queryableRequestService)
         {
-        }
-
-        protected override Task<IQueryable<TestEntity>> WhereAsync(IQueryable<TestEntity> query, DataTableRequest request, DataTableRequest.ColumnRequest column, CancellationToken token = default)
-        {
-            return Task.FromResult(query);
         }
     }
 }
